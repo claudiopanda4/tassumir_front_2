2ff1255b7f20a931c7ebe873108317550b105a79
claudio4panda <claudiopanda4@gmail.com>
2021-10-12 13:00:17 +0100

    edit couple_page

--- resources/views/pagina/couple_page.blade.php
+++ resources/views/pagina/couple_page.blade.php
@@ -48,6 +48,96 @@
                 </p>
             </div>
         </div>
+        <?php 
+        $posts_feed = [
+            [
+                "type" => "img",
+                "page" => "Famosos em Relacionamentos",
+                "time" => "50 min",
+                "link" => "",
+                "text-post" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            ],
+            [
+                "type" => "video",
+                "page" => "Famosos em Relacionamentos",
+                "time" => "50 min",
+                "link" => "",
+                "text-post" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            ],
+            [
+                "type" => "img",
+                "page" => "Famosos em Relacionamentos",
+                "time" => "50 min",
+                "link" => "",
+                "text-post" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            ],
+            [
+                "type" => "video",
+                "page" => "Famosos em Relacionamentos",
+                "time" => "50 min",
+                "link" => "",
+                "text-post" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            ],
+            [
+                "type" => "none",
+                "page" => "Famosos em Relacionamentos",
+                "time" => "50 min",
+                "link" => "",
+                "text-post" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            ],
+            [
+                "type" => "img",
+                "page" => "Famosos em Relacionamentos",
+                "time" => "50 min",
+                "link" => "",
+                "text-post" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            ],
+            [
+                "type" => "img",
+                "page" => "Famosos em Relacionamentos",
+                "time" => "50 min",
+                "link" => "",
+                "text-post" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            ],
+            [
+                "type" => "video",
+                "page" => "Famosos em Relacionamentos",
+                "time" => "50 min",
+                "link" => "",
+                "text-post" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            ],
+            [
+                "type" => "none",
+                "page" => "Famosos em Relacionamentos",
+                "time" => "50 min",
+                "link" => "",
+                "text-post" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            ],
+        ];?>
+        <section class="suggest-slide suggest-slide-page">
+            <header>
+                <h1>Sugestões de Páginas pra Você</h1>
+            </header>
+            <nav class="clearfix">
+                <ul class="clearfix">
+                <?php 
+                $suggest_page = [
+                    [],[],[],[],[],[],[],
+                ];
+                foreach ($suggest_page as $key => $value): ?>
+                    <li class="li-component-suggest clearfix l-5">
+                        <div class="clearfix sugest_component_div">
+                            <div class="sugest_component circle clearfix">
+                                <img class="img-full circle" src="{{asset('storage/img/page/unnamed.jpg')}}">
+                            </div>
+                        </div>
+                        <h1 class="name-suggest text-ellips">Criticando casais</h1>
+                        <a href="" class="follow-suggest"><div>seguir</div></a>
+                    </li>
+                <?php endforeach ?>
+                </ul>
+            </nav>
+        </section>
     </div>
 </div>
 @stop
