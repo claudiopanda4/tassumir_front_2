8f81f66d95cd0d6dcaa72436d8bebab312324587
claudio4panda <claudiopanda4@gmail.com>
2021-10-21 11:05:53 +0100

    edit PerfilController and my_pages

--- resources/views/pagina/pages.blade.php
+++ resources/views/pagina/pages.blade.php
@@ -9,29 +9,19 @@
         <nav>
             <h1 class="title">Minhas Páginas</h1>
             <ul>
-                <?php 
-                    $key = 0;
-                ?>
                 @foreach($allUserPages as $key => $page)
                     <li class="li-component-aside-right clearfix li-my-page">
                         <div class="page-cover circle l-5">
                             <img class="img-full circle" src="../storage/img/page/t30_13_1092985.jpg">
                         </div>
-
-                        <a href="{{route('couple.page_u', $page[$key]['page_uuid']) }}"><h1 class="l-5 name-page text-ellips"> {{ $page[$key]['page_name'] }} </h1></a>
-                        @if($page[$key]['seguidores'] > 1)
-                            <h2 class="l-5 text-ellips"> {{ $page[$key]['seguidores'] }} seguidores</h2>
-
-                        <a href="{{ route('couple.page1', $page_content[$key]->uuid) }}"><h1 class="l-5 name-page text-ellips"> {{ $page['page_name'] }} </h1></a>
+                        <a href="{{route('couple.page_u', $page['page_uuid']) }}"><h1 class="l-5 name-page text-ellips"> {{ $page['page_name'] }} </h1></a>
                         @if($page['seguidores'] > 1)
                             <h2 class="l-5 text-ellips"> {{ $page['seguidores'] }} seguidores</h2>
-
                         @else
-                            <h2 class="l-5 text-ellips"> {{ $page[$key]['seguidores'] }} seguidor</h2>
+                            <h2 class="l-5 text-ellips"> {{ $page['seguidores'] }} seguidor</h2>
                         @endif
                         <!-- <a href="" class="l-5">seguir</a> -->
                     </li>
-                    <?php $key++; ?>
                 @endforeach
             </ul>
         </nav>
