7fd3cec9a10c43e2029aef6e2da6ff03da7c8d7e
Bernarda  Troco <duduborges88@gmail.com>
2021-11-17 14:28:08 +0100

    nothing - domingos

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -85,6 +85,12 @@ class AuthController extends Controller
                     $notificacoes[$a]['tipo']=1;
                     $notificacoes[$a]['id']=$key->identificador_id_destino;
                         break;
+                    case 6:
+                        $notificacoes[$a]['notificacao']=$nome[0];
+                        $notificacoes[$a]['notificacao'].=" esta seguindo a sua pagina";
+                        $notificacoes[$a]['tipo']=1;
+                        $notificacoes[$a]['id']=$key->identificador_id_destino;
+                            break;
                    case 7:
                    $aux= DB::select('select * from identificadors where identificador_id = ?', [$key->identificador_id_destino]);
                    if (sizeof($aux)){$tipo=DB::select('select * from pedido_relacionamentos where pedido_relacionamento_id = ?', [$aux[0]->id]);
@@ -93,6 +99,17 @@ class AuthController extends Controller
                    $notificacoes[$a]['tipo']=7;
                    $notificacoes[$a]['id']=$tipo[0]->uuid;}}
                             break;
+                case 8:
+                            $notificacoes[$a]['notificacao']=" A vossa pagina foi criada com sucesso ";
+                            $notificacoes[$a]['tipo']=1;
+                            $notificacoes[$a]['id']=$key->identificador_id_destino;
+                                break;
+
+              case 9:
+                                $notificacoes[$a]['notificacao']=" o seu pedido de criação de pagina foi negado";
+                                $notificacoes[$a]['tipo']=1;
+                                $notificacoes[$a]['id']=$key->identificador_id_destino;
+                                    break;
 
 
             }
