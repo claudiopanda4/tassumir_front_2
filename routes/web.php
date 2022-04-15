6468bf487d769d8df3d92ec2b19cc7912a9e730f
Bernarda  Troco <duduborges88@gmail.com>
2022-04-15 15:46:54 +0100

    função tassumir video- domingos

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,9 @@ Route::group(['middleware' => 'auth:web1'], function () {
     Route::get('/get_nine_videos_page/', [App\Http\Controllers\PaginaCasalController::class, 'get_nine_videos_page'])->name('couple.get_nine_videos_page');
     /*fim novas rotas pages*/
 
+    /*novas rotas tassumirvideo*/
+    Route::get('/tassumirvideos_final/{id}', [App\Http\Controllers\PostController::class, 'tassumirvideos_final'])->name('post.tassumir.video_final');
+    /*fim das novas rotas tassumirvideo*/
     Route::get('/post_index/{id}', [App\Http\Controllers\AuthController::class, 'post_index'])->name('post_index');
 
     /* siene */
