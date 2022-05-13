8535e9625b15c765d46615529ce937243706bb14
claudiopanda4 <claudiopanda4@gmail.com>
2022-05-13 15:23:44 +0100

    update

--- resources/views/pagina/index.blade.php
+++ resources/views/pagina/index.blade.php
@@ -220,14 +220,14 @@
     let description = $('#p-post_' + $('#ident-post-page').val()).text(), text;
     text = "";
     console.log(description);
-    for (var i = 0; i <= description.length - 1; i++) {
+    /*for (var i = 0; i <= description.length - 1; i++) {
         if (description[i] == '\n') {
             text = text + '\n' + description[i];    
         } else {
             text = text + '' + description[i];
         }
-    }
-    console.log(text);
-    $('#p-post_' + $('#ident-post-page').val()).html(text);
+    }*/
+    console.log(description);
+    $('#p-post_' + $('#ident-post-page').val()).html(description);
 </script>
 @stop
